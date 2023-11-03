46dd035e5dc95c0efcfa5eeba976a77e11284f3d
Joseph Lee <joseph@jc-lab.net>
2023-11-03 10:47:09 +0900

    ci: auto release info

--- cmd/information/main.go
+++ cmd/information/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"flag"
 	"fmt"
 	korea_pki "github.com/jc-lab/korea-pki"
 	"log"
+	"os"
 )
 
 func main() {
+	var flagReleaseInfo bool
+	flag.BoolVar(&flagReleaseInfo, "release-info", false, "print only release info as JSON")
+	flag.Parse()
+
 	core, err := korea_pki.New(context.Background())
 	if err != nil {
 		log.Fatal(err)
@@ -24,26 +31,37 @@ func main() {
 		log.Fatal(err)
 	}
 
-	println("라이선스 목록:")
-	println(licenseInfo.LicensesDocument)
-	println("")
-
-	println("라이브러리 버전: ", licenseInfo.LibraryVersion)
-	println("라이브러리 라이선스 버전: ", licenseInfo.LibraryLicenseVersion)
-
-	appliedLicense := licenseInfo.AppliedLicense
-
-	if appliedLicense != nil {
-		var licenseMaxVer string
-		if appliedLicense.LicenseMaxVersion < 0 {
-			licenseMaxVer = "제한 없음"
-		} else {
-			licenseMaxVer = fmt.Sprintf("%d", appliedLicense.LicenseMaxVersion)
+	if flagReleaseInfo {
+		out, err := json.Marshal(map[string]any{
+			"version":        licenseInfo.LibraryVersion,
+			"licenseVersion": licenseInfo.LibraryLicenseVersion,
+		})
+		if err != nil {
+			log.Fatal(err)
 		}
-		println("적용된 라이선스 버전 상한 : ", licenseMaxVer)
-		println("적용된 라이선스 형태(commercial or opensource) : ", appliedLicense.LicenseType)
-		println("적용된 라이선스 제품: ", appliedLicense.Product)
-		println("적용된 라이선스 사용자: ", appliedLicense.LicenseeName)
-		println("적용된 라이선스 사용자 이메일: ", appliedLicense.LicenseeEmail)
+		os.Stdout.WriteString(string(out))
+	} else {
+		println("라이선스 목록:")
+		println(licenseInfo.LicensesDocument)
+		println("")
+
+		println("라이브러리 버전: ", licenseInfo.LibraryVersion)
+		println("라이브러리 라이선스 버전: ", licenseInfo.LibraryLicenseVersion)
+
+		appliedLicense := licenseInfo.AppliedLicense
+
+		if appliedLicense != nil {
+			var licenseMaxVer string
+			if appliedLicense.LicenseMaxVersion < 0 {
+				licenseMaxVer = "제한 없음"
+			} else {
+				licenseMaxVer = fmt.Sprintf("%d", appliedLicense.LicenseMaxVersion)
+			}
+			println("적용된 라이선스 버전 상한 : ", licenseMaxVer)
+			println("적용된 라이선스 형태(commercial or opensource) : ", appliedLicense.LicenseType)
+			println("적용된 라이선스 제품: ", appliedLicense.Product)
+			println("적용된 라이선스 사용자: ", appliedLicense.LicenseeName)
+			println("적용된 라이선스 사용자 이메일: ", appliedLicense.LicenseeEmail)
+		}
 	}
 }
